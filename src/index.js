b377080dd38fd9e1af9718fc25e35190c35809dd
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-03-15 18:05:04 +0300

    feat: implemented required function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  function reverseEverySecondItem (matrix) {
+    for (let i = 1; i <= (matrix.length - 1); i++) {
+      matrix[i].reverse()
+      i++
+    }
+    return matrix
+  }
+  if (typeof(matrix) !== ('undefined' || null) ){
+    if (matrix.length !== 0) {
+      return reverseEverySecondItem(matrix).toString().split(',')
+    } else {
+      return [] //matrix is undefined
+    }
+  } else {
+    return [] //matrix is undefined
+  } 
 }
